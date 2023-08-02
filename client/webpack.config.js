393f382c3ae709dca479a3b363bb40ff35848a3b
Thomas Henderson <trhenderson303@gmail.com>
2023-08-02 17:38:51 -0600

    troubleshoot

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,48 +1,44 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "J.A.T.E",
+        title: "TODOs List",
       }),
-
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
-
       new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: "Just Another Text Editor",
-        short_name: "J.A.T.E",
-        description: "Take notes with Javascript syntax highlighting",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
+        name: "My Progressive Web App",
+        short_name: "MyPWA",
+        description: "My awesome Progressive Web App!",
+        background_color: "#ffffff",
+        crossorigin: "use-credentials",
         start_url: "/",
         publicPath: "/",
+        //can be null, use-credentials or anonymous
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
             destination: path.join("assets", "icons"),
           },
         ],
@@ -52,13 +48,13 @@ module.exports = () => {
     module: {
       rules: [
         {
-          test: /\.css$/i,
+          test: /\.css$/,
           use: ["style-loader", "css-loader"],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-
+          // We use babel-loader in order to use ES6.
           use: {
             loader: "babel-loader",
             options: {
